6e6ec85d09fc37d5ca9638ef5def588c895760c2
jaydewong <jayde_wong@yahoo.com>
2022-04-26 15:34:14 -0700

    added a list view into xml

--- app/src/main/java/com/example/zooapp/MainActivity.java
+++ app/src/main/java/com/example/zooapp/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //follow the lab and make an adapter + use recycler view?
+        //i added a listview into the xml
     }
 }
