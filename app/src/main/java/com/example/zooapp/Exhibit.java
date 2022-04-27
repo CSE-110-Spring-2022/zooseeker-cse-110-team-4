cd455845b08914d37e4a743c092de6edf51e0655
Duke Wu <d3wu@ucsd.edu>
2022-04-27 15:31:36 -0700

    JSON reader

--- app/src/main/java/com/example/zooapp/Exhibit.java
+++ app/src/main/java/com/example/zooapp/Exhibit.java
@@ -1,9 +1,21 @@
 package com.example.zooapp;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Entity(tableName = "exhibit_list")
@@ -21,7 +33,24 @@ public class Exhibit {
         this.tags = tags;
     }
 
-    public String[] getTags() {
-        return tags;
+    public static List<Exhibit> loadJSON(Context context, String path) {
+        try {
+            InputStream inputStream = context.getAssets().open(path);
+            Reader reader = new InputStreamReader(inputStream);
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<Exhibit>>(){}.getType();
+            return gson.fromJson(reader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Exhibit{" +
+                "name='" + name + '\'' +
+                ", tags=" + Arrays.toString(tags) +
+                '}';
     }
 }
--- app/src/main/java/com/example/zooapp/MainActivity.java
+++ app/src/main/java/com/example/zooapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.zooapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Exhibit> exhibitList = Exhibit.loadJSON(this, "exhibit_list.json");
+        Log.d("Exhibit List Activity", exhibitList.toString());
     }
 }
