cc66480ae3150f00354f49ec53e4ec2daf811f8f
Duke Wu <d3wu@ucsd.edu>
2022-04-26 20:16:56 -0700

    ZooSeekerDatabase for Animals in Zoo

--- app/src/main/java/com/example/zooapp/ZooSeekerDatabase.java
+++ app/src/main/java/com/example/zooapp/ZooSeekerDatabase.java
@@ -1,3 +1,9 @@
 package com.example.zooapp;
-public class ZooSeekerDatabase {
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Animal.class}, version = 1)
+public abstract class ZooSeekerDatabase extends RoomDatabase {
+    public abstract DaoName DaoName();
 }
