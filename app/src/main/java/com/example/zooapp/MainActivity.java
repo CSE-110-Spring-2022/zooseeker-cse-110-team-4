0519c9d4caad63f0f0332730f8ec37cacee4a3fb
brandonele <bele@ucsd.edu>
2022-04-28 15:33:05 -0700

    Planned UI with counter, hardcoded w/ animals

--- app/src/main/java/com/example/zooapp/MainActivity.java
+++ app/src/main/java/com/example/zooapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zooapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +10,7 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,10 +18,30 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public final List<String> animalList = Arrays.asList();
+    public final SampleAnimal tiger = new SampleAnimal("Tiger");
+    public final SampleAnimal bear = new SampleAnimal("Polar Bear");
+    public final SampleAnimal bird = new SampleAnimal("Toucan");
+    public final SampleAnimal lion = new SampleAnimal("Lion");
+    public final SampleAnimal walrus = new SampleAnimal("Walrus");
+
+    public final List<SampleAnimal> samplelist = Arrays.asList(tiger,bear,bird,lion,walrus);
+    public RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PlannedAnimalAdapter adaptertest = new PlannedAnimalAdapter();
+        adaptertest.setHasStableIds(true);
+
+        recyclerView = findViewById(R.id.planned_animals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adaptertest);
+
+        adaptertest.setAnimalList(samplelist);
+
+        TextView count = findViewById(R.id.added_counter);
+        count.setText("(" +Integer.toString(samplelist.size()) +")");
 
         SearchView searchBar = findViewById(R.id.searchBar);
         ListView listView = findViewById(R.id.listView);
