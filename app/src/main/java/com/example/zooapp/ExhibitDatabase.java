c33d5d6054d1146c9a5dba4bca9829b4cfc47501
Duke Wu <d3wu@ucsd.edu>
2022-04-28 14:24:33 -0700

    Updated ZooNode Removed Exhibit

--- app/src/main/java/com/example/zooapp/ExhibitDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.zooapp;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Exhibit.class}, version = 1)
-public abstract class ExhibitDatabase extends RoomDatabase {
-    public abstract ExhibitDao ExhibitDao();
-}
--- app/src/main/java/com/example/zooapp/MainActivity.java
+++ app/src/main/java/com/example/zooapp/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Exhibit> exhibitList = Exhibit.loadJSON(this, "exhibit_list.json");
-        Log.d("Exhibit List Activity", exhibitList.toString());
-
         List<ZooNode> zooNodeList = ZooNode.loadJSON(this, "zoo_node_list.json");
         Log.d("Zoo Node List Activity", zooNodeList.toString());
     }
