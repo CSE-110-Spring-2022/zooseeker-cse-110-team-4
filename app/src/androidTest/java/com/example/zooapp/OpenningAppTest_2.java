efaa8b9ea56db080981d1b386d35aa3add60594b
Huy <chn019@ucsd.edu>
2022-05-01 16:13:02 -0700

    another OpeninngAppTest

--- app/src/androidTest/java/com/example/zooapp/OpenningAppTest_2.java
+++ app/src/androidTest/java/com/example/zooapp/OpenningAppTest_2.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class OpenningAppTest_2 {
-
+//
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
