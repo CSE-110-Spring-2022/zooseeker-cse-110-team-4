b0b600f4ceb7c7c00696aa1b43b732e67e69563e
jaydewong <jayde_wong@yahoo.com>
2022-05-03 15:20:14 -0700

    added directions page layout

--- app/src/main/java/com/example/zooapp/DirectionsActivity.java
+++ app/src/main/java/com/example/zooapp/DirectionsActivity.java
@@ -1,15 +1,14 @@
 package com.example.zooapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class DirectionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_directions);
     }
-
 }
