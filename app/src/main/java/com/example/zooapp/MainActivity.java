04aede4ee7cd5277e414327e0070f26899a63a41
Duke Wu <d3wu@ucsd.edu>
2022-05-04 17:56:13 -0700

    Style for Main

--- app/src/main/java/com/example/zooapp/MainActivity.java
+++ app/src/main/java/com/example/zooapp/MainActivity.java
@@ -37,19 +37,31 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This is the main activity, where the app initially loads into
+ */
 public class MainActivity extends AppCompatActivity{
+    // Public fields
     public List<ZooNode> userExhibits;
     public RecyclerView recyclerView;
+    public ActionBar actionBar;
+
+    // Private fields
     private PlannedAnimalAdapter plannedAnimalAdapter;
     private TextView userExhibitsSize;
-    public ActionBar actionBar;
     private static final int REQUEST_USER_CHOSEN_ANIMAL = 0;
 
+    /**
+     * Method for onCreate of the activity
+     *
+     * @param savedInstanceState State of activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set the Title Bar to Zoo Seeker
         actionBar = getSupportActionBar();
         actionBar.setTitle("Zoo Seeker");
 
@@ -57,10 +69,14 @@ public class MainActivity extends AppCompatActivity{
 
         setUpRecyclerView();
 
+        // Added counter for user to see
         userExhibitsSize = findViewById(R.id.added_counter);
         userExhibitsSize.setText("(" + userExhibits.size() + ")");
     }
 
+    /**
+     * Sets up the recycler view to display the animals that the user has currently selected
+     */
     private void setUpRecyclerView() {
         plannedAnimalAdapter = new PlannedAnimalAdapter();
         plannedAnimalAdapter.setAnimalList(userExhibits);
@@ -69,18 +85,32 @@ public class MainActivity extends AppCompatActivity{
         recyclerView.setAdapter(plannedAnimalAdapter);
     }
 
+    /**
+     * Used to start the search activity, waits for a click result from the search activity
+     *
+     * @param requestCode Code for when the result is done
+     * @param resultCode Code for how the result went
+     * @param data The intent of the activity we are returning from
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if( requestCode == REQUEST_USER_CHOSEN_ANIMAL && resultCode == Activity.RESULT_OK ) {
             Gson gson = new Gson();
             Type type = new TypeToken<List<ZooNode>>(){}.getType();
-            userExhibits = gson.fromJson(data.getStringExtra("userExhibitsJSONUpdated"), type);
+            userExhibits = gson.fromJson(data.getStringExtra("userExhibitsJSONUpdated"),
+                    type);
             plannedAnimalAdapter.setAnimalList(userExhibits);
             updateCount();
         }
     }
 
+    /**
+     * Creates the custom menu bar
+     *
+     * @param menu Menu
+     * @return True for creating the menu bar
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -89,6 +119,12 @@ public class MainActivity extends AppCompatActivity{
         return true;
     }
 
+    /**
+     * Checks when an item on the menu has been clicked
+     *
+     * @param item Item that has been clicked
+     * @return Result of that item being clicked
+     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         Log.d("Menu Click", "Search has been clicked");
@@ -99,10 +135,18 @@ public class MainActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Method used to update the selected animals list size
+     */
     public void updateCount() {
         userExhibitsSize.setText("(" + userExhibits.size() + ")");
     }
 
+    /**
+     * For when the plan button is clicked
+     *
+     * @param view The current view
+     */
     public void onPlanButtonClicked(View view) {
         Intent intent = new Intent(this, DirectionsActivity.class);
         Gson gson = new Gson();
