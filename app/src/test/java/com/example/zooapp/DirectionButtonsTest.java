47930556f5f1235f1e277de720badcbffd8567c4
jaydewong <jayde_wong@yahoo.com>
2022-05-07 21:11:34 -0700

    direction button tests pass

--- app/src/test/java/com/example/zooapp/DirectionButtonsTest.java
+++ app/src/test/java/com/example/zooapp/DirectionButtonsTest.java
@@ -25,6 +25,7 @@ import static org.robolectric.RuntimeEnvironment.getApplication;
 import com.google.gson.Gson;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -32,16 +33,15 @@ import java.util.List;
 public class DirectionButtonsTest {
     Context context;
     ZooNodeDao dao;
-    /*
-    1. test initial visibility - passed
-    2. test alert message pops up on next -
-    3. previous appears and disappears properly -
-    4. alert message on plan if empty list - passed
-     */
 
-    @Rule
-    public ActivityScenarioRule directionsRule = new ActivityScenarioRule(DirectionsActivity.class);
 
+    String[] tags = {"lions", "cats","mammal", "africa"};
+    ZooNode animal = new ZooNode("lions", "exhibit", "Lions",tags);
+
+    String[] tags2 = {"elephant", "mammal", "africa"};
+    ZooNode animal2 = new ZooNode("elephant_odyssey", "exhibit", "Elephant Odyssey",tags2);
+
+    //Set up the database of animals in the zoo
     @Before
     public void setup() {
         context = ApplicationProvider.getApplicationContext();
@@ -51,6 +51,7 @@ public class DirectionButtonsTest {
                 .ZooNodeDao();
         List<ZooNode> allZooNodes = ZooNode.loadJSON(context, "sample_node_info.json");
         dao.insertAll(allZooNodes);
+
     }
 
     /**
@@ -60,31 +61,33 @@ public class DirectionButtonsTest {
     @Test
     public void testInitialButtonVisibility(){
 
-//        //go to directions activity
+        //Start in Directions Activity
         ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
         scenario.moveToState(Lifecycle.State.RESUMED);
 
-        //next button should be visible
-        //previous button should not be visible
         scenario.onActivity(activity -> {
 
             String[] tags = {"mammal"};
             ZooNode animal = new ZooNode("tiger", "exhibit", "name",tags);
             activity.userExhibits.add(animal);
 
+            assertEquals(1, activity.userExhibits.size());
+
+            //Click Plan button, move to DirectionActivity view
             Button plan = activity.findViewById(R.id.plan_button);
             plan.performClick();
-
             activity.setContentView(R.layout.activity_directions);
 
+            //Next should be visible, Previous should be invisible
             Button next = activity.findViewById(R.id.next_button);
             Button previous = activity.findViewById(R.id.previous_button);
 
             assertEquals(next.getVisibility(), View.VISIBLE);
             assertEquals(previous.getVisibility(), View.INVISIBLE);
         });
+
     }
 
     /**
@@ -92,114 +95,99 @@ public class DirectionButtonsTest {
      */
     @Test
     public void testPlanClickedEmptyList() {
+        //Start in MainActivity
         ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
         scenario.moveToState(Lifecycle.State.RESUMED);
 
         scenario.onActivity(activity -> {
-            //planned list starts at 0
+            //No animals have been added to the planned list
             Button plan = activity.findViewById(R.id.plan_button);
             plan.performClick();
             assertEquals(true, activity.alertMessage.isShowing());
         });
-
-        //go to directions activity
-//        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
-//        scenario.moveToState(Lifecycle.State.CREATED);
-//        scenario.moveToState(Lifecycle.State.STARTED);
-//        scenario.moveToState(Lifecycle.State.RESUMED);
-//
-//        //next button should be visible
-//        //previous button should not be visible
-//        scenario.onActivity(activity -> {
-//            activity.userExhibits.add(dao.getById(0));
-//
-//            Button planButton = activity.findViewById(R.id.plan_button);
-//            planButton.performClick();
-//
-//            Button nextButton = activity.findViewById(R.id.next_button);
-//            Button previousButton = activity.findViewById(R.id.previous_button);
-//
-//            assertEquals(nextButton.getVisibility(), View.VISIBLE);
-//            assertEquals(previousButton.getVisibility(), View.INVISIBLE);
-//        });
-
+        scenario.close();
     }
 
     /**
-     * Test when clicking the next button at the end of the route, an alert pops up on screen
+     * Test Next and Previous with a planned list of two animals
+     * On first animal, Previous should be invisible
+     * On last animal, clicking Next should cause an alert to appear
      */
     @Test
-    public void testAlertMessageOnNextClicked(){
+    public void testButtonsTwoAnimals(){
 
+        //Start in MainActivity
         ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
         scenario.moveToState(Lifecycle.State.RESUMED);
 
-        //next button should be visible
-        //previous button should not be visible
         scenario.onActivity(activity -> {
 
-            String[] tags = {"lions", "cats","mammal", "africa"};
-            ZooNode animal = new ZooNode("lions", "exhibit", "Lions",tags);
+            //Add two animals to planned list
             activity.userExhibits.add(animal);
-
-            String[] tags2 = {"elephant", "mammal", "africa"};
-            ZooNode animal2 = new ZooNode("elephant_odyssey", "exhibit", "Elephant Odyssey",tags2);
             activity.userExhibits.add(animal2);
+            assertEquals(2, activity.userExhibits.size());
 
-            Button plan = activity.findViewById(R.id.plan_button);
-            plan.performClick(); //move to directions activity ?
+            //Set up intent to move to DirectionsActivity
+            Intent intent = new Intent(ApplicationProvider.getApplicationContext(), DirectionsActivity.class);
+            Gson gson = new Gson();
+            intent.putExtra("ListOfAnimals", gson.toJson(activity.userExhibits));
 
-            ActivityScenario<DirectionsActivity> scenario2 = ActivityScenario.launch(DirectionsActivity.class);
+            //Start DirectionsActivity
+            ActivityScenario<DirectionsActivity> scenario2 = ActivityScenario.launch(intent);
             scenario2.moveToState(Lifecycle.State.CREATED);
             scenario2.moveToState(Lifecycle.State.STARTED);
             scenario2.moveToState(Lifecycle.State.RESUMED);
 
-                    scenario2.onActivity(activity2 -> {
-                        Button next = activity2.findViewById(R.id.next_button);
-                        next.performClick(); //TODO will not work while i can only access methods in Main - cant access onNextClicked
-                        next.performClick();
-                        assertEquals(true, activity2.alertMessage.isShowing());
+            scenario2.onActivity(activity2 -> {
+                Button next = activity2.findViewById(R.id.next_button);
+                Button previous = activity2.findViewById(R.id.previous_button);
 
-                    });
+                //Two animals in planned list, but path should be length four including entrance and exit
+                assertEquals(2, activity2.userExhibits.size());
+                assertEquals(4, activity2.userListShortestOrder.size());
+
+                //currIndex starts at first animal, index at 0
+                //Previous should be invisible, Next should be visible
+                assertEquals(0, activity2.currIndex);
+                assertEquals(View.INVISIBLE, previous.getVisibility());
+                assertEquals(View.VISIBLE, next.getVisibility());
+
+                //click Next to move to the second animal, currIndex at 1
+                next.performClick();
+                assertEquals(1, activity2.currIndex);
+                assertEquals(View.VISIBLE, previous.getVisibility());
+                assertEquals(View.VISIBLE, next.getVisibility());
+
+                //click Previous to return to first animal, Previous button is invisible
+                previous.performClick();
+                assertEquals(View.INVISIBLE, previous.getVisibility());
+                assertEquals(View.VISIBLE, next.getVisibility());
+
+                //click Next to move to second animal, currIndex at 1
+                next.performClick();
+                assertEquals(1, activity2.currIndex);
+
+                //click Next to move to exit, currIndex at 2
+                next.performClick();
+                assertEquals(2, activity2.currIndex);
+
+                //click Next to get an alert that the route is complete.
+                next.performClick();
+                assertEquals(true, activity2.alertMessage.isShowing());
+                assertEquals(View.VISIBLE, next.getVisibility());
+
+
+
+            });
+
         });
-    }
 
-    /**
-     * When we reach the first animal on the route, the previous button should disappear again.
-     */
-//    @Test
-//    public void testPreviousButtonDisappears(){
-//        //TODO
-//        //go to directions activity
-//        ActivityScenario<DirectionsActivity> scenario = ActivityScenario.launch(DirectionsActivity.class);
-//        scenario.moveToState(Lifecycle.State.CREATED);
-//        scenario.moveToState(Lifecycle.State.STARTED);
-//        scenario.moveToState(Lifecycle.State.RESUMED);
-//
-//        //next button should be visible
-//        //previous button should not be visible
-//        scenario.onActivity(activity -> {
-//            Button next = activity.findViewById(R.id.next_button);
-//            Button previous = activity.findViewById(R.id.previous_button);
-//
-//            //move onto the next animal
-//            next.performClick();
-//
-//            //check if previous has appeared
-//            assertEquals(View.VISIBLE, previous.getVisibility());
-//
-//            //move back to first animal
-//            previous.performClick();
-//
-//            //check if previous has disappeared
-//            assertEquals(View.INVISIBLE, previous.getVisibility());
-//
-//        });
- //   }
+
+    }
 
 //    @Test
 //    public void useAppContext() {
