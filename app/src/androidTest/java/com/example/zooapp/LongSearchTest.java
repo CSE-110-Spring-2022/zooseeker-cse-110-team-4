6ae640a01e2b7b491fc5a5cca8c7c374e9c2f626
Duke Wu <d3wu@ucsd.edu>
2022-05-07 22:34:21 -0700

    FullScenarioTest and LongSearchTest

--- app/src/androidTest/java/com/example/zooapp/LongSearchTest.java
+++ app/src/androidTest/java/com/example/zooapp/LongSearchTest.java
@@ -6,7 +6,6 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
@@ -34,13 +33,13 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class FullScenarioTest {
+public class LongSearchTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void fullScenarioTest() {
+    public void longSearchTest() {
         ViewInteraction actionMenuItemView = onView(
                 allOf(withId(R.id.actions_search), withContentDescription("Search"),
                         childAtPosition(
@@ -61,25 +60,6 @@ public class FullScenarioTest {
                                 1),
                         isDisplayed()));
         appCompatImageButton.perform(click());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.plan_button), withText("plan"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(android.R.id.button1), withText("Ok"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        appCompatButton2.perform(scrollTo(), click());
 
         ViewInteraction actionMenuItemView2 = onView(
                 allOf(withId(R.id.actions_search), withContentDescription("Search"),
@@ -111,10 +91,10 @@ public class FullScenarioTest {
                                                 1)),
                                 0),
                         isDisplayed()));
-        searchAutoComplete.perform(replaceText("fox"), closeSoftKeyboard());
+        searchAutoComplete.perform(replaceText("mammal"), closeSoftKeyboard());
 
         ViewInteraction searchAutoComplete2 = onView(
-                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("fox"),
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("mammal"),
                         childAtPosition(
                                 allOf(withClassName(is("android.widget.LinearLayout")),
                                         childAtPosition(
@@ -129,7 +109,7 @@ public class FullScenarioTest {
                         childAtPosition(
                                 withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
                                 0)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
 
         ViewInteraction actionMenuItemView3 = onView(
                 allOf(withId(R.id.actions_search), withContentDescription("Search"),
@@ -161,10 +141,10 @@ public class FullScenarioTest {
                                                 1)),
                                 0),
                         isDisplayed()));
-        searchAutoComplete3.perform(replaceText("mammal"), closeSoftKeyboard());
+        searchAutoComplete3.perform(replaceText("africa"), closeSoftKeyboard());
 
         ViewInteraction searchAutoComplete4 = onView(
-                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("mammal"),
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("africa"),
                         childAtPosition(
                                 allOf(withClassName(is("android.widget.LinearLayout")),
                                         childAtPosition(
@@ -179,7 +159,7 @@ public class FullScenarioTest {
                         childAtPosition(
                                 withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
                                 0)));
-        recyclerView2.perform(actionOnItemAtPosition(3, click()));
+        recyclerView2.perform(actionOnItemAtPosition(0, click()));
 
         ViewInteraction actionMenuItemView4 = onView(
                 allOf(withId(R.id.actions_search), withContentDescription("Search"),
@@ -211,10 +191,10 @@ public class FullScenarioTest {
                                                 1)),
                                 0),
                         isDisplayed()));
-        searchAutoComplete5.perform(replaceText("africa"), closeSoftKeyboard());
+        searchAutoComplete5.perform(replaceText("lion"), closeSoftKeyboard());
 
         ViewInteraction searchAutoComplete6 = onView(
-                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("africa"),
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("lion"),
                         childAtPosition(
                                 allOf(withClassName(is("android.widget.LinearLayout")),
                                         childAtPosition(
@@ -230,132 +210,6 @@ public class FullScenarioTest {
                                 withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
                                 0)));
         recyclerView3.perform(actionOnItemAtPosition(0, click()));
-
-        ViewInteraction actionMenuItemView5 = onView(
-                allOf(withId(R.id.actions_search), withContentDescription("Search"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(androidx.appcompat.R.id.action_bar),
-                                        1),
-                                0),
-                        isDisplayed()));
-        actionMenuItemView5.perform(click());
-
-        ViewInteraction recyclerView4 = onView(
-                allOf(withId(R.id.animalListView),
-                        childAtPosition(
-                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                0)));
-        recyclerView4.perform(actionOnItemAtPosition(3, click()));
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.plan_button), withText("plan"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton3.perform(click());
-
-        ViewInteraction appCompatButton4 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton4.perform(click());
-
-        ViewInteraction appCompatButton5 = onView(
-                allOf(withId(R.id.previous_button), withText("Previous"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatButton5.perform(click());
-
-        ViewInteraction appCompatButton6 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton6.perform(click());
-
-        ViewInteraction appCompatButton7 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton7.perform(click());
-
-        ViewInteraction appCompatButton8 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton8.perform(click());
-
-        ViewInteraction appCompatButton9 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton9.perform(click());
-
-        ViewInteraction appCompatButton10 = onView(
-                allOf(withId(R.id.previous_button), withText("Previous"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatButton10.perform(click());
-
-        ViewInteraction appCompatButton11 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton11.perform(click());
-
-        ViewInteraction appCompatButton12 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton12.perform(click());
-
-        ViewInteraction appCompatButton13 = onView(
-                allOf(withId(android.R.id.button1), withText("Ok"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        appCompatButton13.perform(scrollTo(), click());
     }
 
     private static Matcher<View> childAtPosition(
